9d53ebeb0097a4803458a58efd38570b69bd9a78
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-19 22:09:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def selects_most_prominent_color_and_returns_with_count
   #{}"SELECT color, COUNT() FROM bears WHERE color LIKE 'MAX(color)';"
   #{}"SELECT MAX(color), AVG(color) FROM bears"
   #{}"SELECT MAX(color), COUNT('DISTINCT MAX(color)') FROM bears;"
-  "SELECT MAX(color), COUNT(bears.color) FROM bears;"
+  "SELECT color, COUNT(bears.color) FROM bears;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
